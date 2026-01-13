b3fababfc97cd07d2e996c74b618143e11a43775
LuanCKRG <luancamposck@gmail.com>
2026-01-13 02:04:10 -0300

    refactor(imports): change imports to use alias path

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
-import { App } from "./app"
-import "./index.css"
+import { App } from "@/app"
+import "@/index.css"
 
 const rootEl = document.getElementById("root")
 
