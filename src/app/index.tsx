c2299180cb7fec4f63899bdb9349559231730863
LuanCKRG <luancamposck@gmail.com>
2026-01-16 23:38:28 -0300

    feat(app): integrate CreateTagForm into the main application layout

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,14 @@
+import { CreateTagForm } from "@/features/job-tags/ui/components/create-tag-form"
+
 export const App = () => {
 	return (
 		<>
 			<header className="w-full border-b-2">
 				<div>Hello World</div>
 			</header>
+			<main className="flex w-full justify-center px-6 py-10">
+				<CreateTagForm />
+			</main>
 		</>
 	)
 }
